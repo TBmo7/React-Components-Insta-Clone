ec42c4d6e15cc4edc99ef5a67d8346b28159021a
Moore <Troy.Moore@unisys.com>
2020-01-31 19:03:39 -0500

    finished at work

--- src/App.js
+++ src/App.js
@@ -13,10 +13,19 @@ import dummyData from "./dummy-data.js"
 // import dummydata
 
 const App = () => {
+  //console.log({dummyData});
+
   // set up state for the dummy data and pass to your PostsPage
   return (
+
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+        <SearchBar/>
+        <PostsPage />
+
+      {/* Add components here  and pass props where appropriate */
+
+
+      }
     </div>
   );
 };
